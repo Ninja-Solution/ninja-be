9d939ae4f7c40c1622fecbb88af2f6abf772a14f
장원진 <wkd3828@gmail.com>
2023-07-19 17:21:26 +0900

    사건 등록 수정

--- src/main/java/ninja/be/entity/enums/IncidentType.java
+++ src/main/java/ninja/be/entity/enums/IncidentType.java
@@ -1,10 +1,19 @@
 package ninja.be.entity.enums;
 
 public enum IncidentType {
-    LANDSLIDE,
-    FLOOD,
-    INUNDATION,
-    WILDFIRE,
-    FIRE,
-    EARTHQUAKE,
+    LANDSLIDE("가뭄"),
+    FLOOD("홍수"),
+    INUNDATION("침수"),
+    WILDFIRE("산불"),
+    FIRE("화재"),
+    EARTHQUAKE("지진"),
+    ;
+
+    private final String incident;
+    IncidentType(String incident) {
+        this.incident=incident;
+    }
+    public String getIncident(){
+        return this.incident;
+    }
 }
