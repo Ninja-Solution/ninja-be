237c1c735926e5c57613ca51dadae53c52a5cd1f
장원진 <wkd3828@gmail.com>
2023-07-21 10:08:03 +0900

    업데이트 수정

--- src/main/java/ninja/be/service/UserService.java
+++ src/main/java/ninja/be/service/UserService.java
@@ -8,8 +8,10 @@ import ninja.be.entity.enums.Authority;
 import ninja.be.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
